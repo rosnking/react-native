6614097a562c582ac829d9a384b1028a44540d49
Lasisi kehinde <lasisikehinde153@gmail.com>
2020-05-05 03:02:18 +0100

    First React Native demo

--- App.js
+++ App.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
 
 export default function App() {
+
+   const [count, setCount] = useState(0)
+   const handlePress = () =>{
+      setCount(count + 1)
+   }
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
+  <Text style={styles.text}> Increase count by {count}</Text>
+      <Button title="Press me" onPress={handlePress}/>
+      <Button title="Reset" onPress={()=> setCount(0)}/>
     </View>
   );
 }
@@ -16,4 +24,9 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text: {
+      color: '#ff652f',
+      fontSize: 40,
+      marginTop: 20,
+  }
 });
